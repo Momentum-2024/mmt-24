90d94fe6725856fd1a3d343cda13f9755be47f1c
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-07-18 13:58:21 +0530

    Events Page added

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -11,27 +11,10 @@ export default function Events({ }) {
   const [eventFilter, setEventFilter] = useState("All");
   const [searchQuery, setSearchQuery] = useState("");
 
-  const categories = [
-    "Music",
-    "Dance",
-    "Entertainment",
-    "Quizzing",
-    "Fun Activities",
-    "Fashion",
-    "Culinary",
-    "Sports",
-    "Esports",
-    "Technical",
-    "Non Technical",
-    "Creative",
-    "Comic",
-    "Finance",
-    "Literary",
-  ];
   return (
     <div className="h-full min-h-screen bg-[#030919] text-white px-10 ">
-      <EventsSearchBar/>
-      <div className="h-full mt-5 overflow-y-scroll grid grid-flow-col justify-around">  
+      <EventsSearchBar eventFilter={eventFilter} setEventFilter={setEventFilter}/>
+      <div className="h-full mt-5 overflow-y-scroll grid grid-cols-5 gap-10 ">  
         <Image src="/event-poster.png" height={200} width={200} alt='p1'/>
         <Image src="/event-poster.png" height={200} width={200} alt='p1'/>
         <Image src="/event-poster.png" height={200} width={200} alt='p1'/>
--- src/components/EventsSearchBar.tsx
+++ src/components/EventsSearchBar.tsx
@@ -1,32 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoMdSearch } from "react-icons/io";
 import { FaFilter } from "react-icons/fa";
+import CustomDrawer from './CustomDrawer';
 
-function EventsSearchBar() {
+function EventsSearchBar({eventFilter, setEventFilter}) {
+    const [drawerOpen, setDrawerOpen]= useState(false);
   return (
-    <div className='pt-[100px] sticky top-0 bg-[#030919] '>
-        <div className='flex items-center space-x-3 '>
-            <div className="w-[40%] bg-[#374151] h-11 flex items-center px-3 py-2 rounded-md">
-                <IoMdSearch size={25}/>
-                <input type="text" name="" id="" 
-                    className='bg-transparent w-full outline-none px-2'
-                    placeholder='Search events'
-                />
+    <>
+        <div className='pt-[100px] sticky top-0 bg-[#030919] '>
+            <div className='flex items-center space-x-3 '>
+                <div className="w-[40%] bg-[#374151] h-11 flex items-center px-3 py-2 rounded-md">
+                    <IoMdSearch size={25}/>
+                    <input type="text" name="" id="" 
+                        className='bg-transparent w-full outline-none px-2'
+                        placeholder='Search events'
+                    />
+                    <button
+                        className=" bg-green-500 px-5 py-1 rounded-md hover:bg-green-600"
+                    >
+                        Search
+                    </button>    
+                </div>
                 <button
-                    className=" bg-green-500 px-5 py-1 rounded-md hover:bg-green-600"
+                    className=" bg-purple-500 h-10 px-5 rounded-md hover:bg-purple-600 flex items-center space-x-3"
+                    onClick={()=>{
+                        setDrawerOpen(true);
+                    }}
                 >
-                    Search
-                </button>    
+                    <FaFilter size={12}/>
+                    <span>Categories</span>
+                </button>  
             </div>
-            <button
-                className=" bg-purple-500 h-10 px-5 rounded-md hover:bg-purple-600 flex items-center space-x-3"
-            >
-                <FaFilter size={12}/>
-                <span>Categories</span>
-            </button>  
+            <hr className="mt-7 opacity-[0.3] pb-4"/>
         </div>
-        <hr className="mt-7 opacity-[0.3] pb-4"/>
-    </div>
+        <CustomDrawer drawerOpen={drawerOpen} setDrawerOpen={setDrawerOpen} eventFilter={eventFilter} setEventFilter={setEventFilter}/>
+    </>
   )
 }
 
